e7f3352b939ed512b0d057ed3bfa7080722dc2f5
ys Asesa Nitaicandra <abinashshaw16@gmail.com>
2026-05-27 13:44:59 +0530

    Completed The User Microservice

--- src/main/java/com/learn/user/entitities/User.java
+++ src/main/java/com/learn/user/entitities/User.java
@@ -1,11 +1,11 @@
 package com.learn.user.entitities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -20,6 +20,7 @@ public class User {
     @Column(unique = true)
     private String email;
     private String about;
-
+    @Transient
+    List<Rating> ratings = new ArrayList<>();
 
 }
